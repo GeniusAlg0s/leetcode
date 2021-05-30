38c7fe41d8ee3633ff268490cb498d47c0c0f4ce
PatWil <patrick.williams@tlgcohort.com>
2021-05-30 13:03:34 -0700

    reverse interger leetcode problem

---
+++ src/ReverseInteger.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class ReverseInteger {
+    public int reverse(int x) {
+        StringBuilder alomstThere = new StringBuilder();
+        int y = Math.abs(x);
+        String xAsString = String.valueOf(y);
+        alomstThere.append(xAsString);
+        String reversed = alomstThere.reverse().toString();
+        try{
+            if(x<0){
+                System.out.println(Integer.parseInt(reversed)* -1);
+            }else {
+                System.out.println(Integer.parseInt(reversed));
+            }
+            return (x <0) ? Integer.parseInt(reversed)* -1: Integer.parseInt(reversed);
+        }catch (NumberFormatException error){
+            return 0;
+        }
+    }
+
+
+    public static void main(String[]args){
+        ReverseInteger user = new ReverseInteger();
+        user.reverse(-456789);
+    }
+}
