fa2e1c86f006b335f5887cc7b4921af37f39923b
PatWil <patrick.williams@tlgcohort.com>
2021-05-31 07:25:45 -0700

    palindrome from leetcode

---
+++ src/Palindrome.java
@@ -0,0 +1,18 @@
+class Palindrome {
+
+    public boolean isPalindrome(int x) {
+        StringBuilder pCheck = new StringBuilder().append(x);
+        System.out.println(pCheck);
+        pCheck.reverse();
+        System.out.println(pCheck);
+        String input = String.valueOf(x);
+        System.out.println(input);
+        System.out.println((input.equals(pCheck.toString()))? true:false);
+        return (input.equals(pCheck.toString()))? true:false;
+
+    }
+    public static void main(String []args){
+        Palindrome users = new Palindrome();
+        users.isPalindrome(-12121);
+    }
+}
