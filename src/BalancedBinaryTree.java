ceda28984f69e28e7c68e0cfd35ff4c5f557db31
GeniusAlg0s <patrick.williams@tlgcohort.com>
2021-06-07 07:14:46 -0700

    find if binary tree is balanced alot of logic cam from research (geeksforgeeks)

---
+++ src/BalancedBinaryTree.java
@@ -0,0 +1,31 @@
+class BalancedBinaryTree {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+  public int height(TreeNode node){
+        if(node == null)return 0;
+
+        return 1 + Math.max(height(node.left),height(node.right));
+  }
+    public boolean isBalanced(TreeNode root) {
+        if(root == null)return true;
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        if(Math.abs(left-right) <=1 && isBalanced(root.left) && isBalanced(root.right)){
+            return true;
+        }
+        return false;
+    }
+}
