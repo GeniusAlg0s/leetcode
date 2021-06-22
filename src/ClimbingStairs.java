11436fd89e17f59ee0674aa2b423ff35837501cd
GeniusAlg0s <plwg123@outlook.com>
2021-06-22 08:02:57 -0700

    leetcode climbing stairs  if (n == 0) {

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,22 @@
+class ClimbingStairs {
+    public static int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int f = 1;
+        int s = 2;
+        for (int i = 3; i <= n; i++) {
+            int t = f + s;
+            f = s;
+            s = t;
+        }
+        return s;
+    }
+    public static void main(String[]args){
+        System.out.println(climbStairs(9));
+    }
+
+}
