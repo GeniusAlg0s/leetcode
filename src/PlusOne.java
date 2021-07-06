b603fe210589c801e0e6c5ffe547948c9e4697ac
GeniusAlg0s <plwg123@outlook.com>
2021-07-06 11:22:40 -0700

    leetcode got behinds on commits

--- src/PlusOne.java
+++ src/PlusOne.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;class PlusOne {
+class PlusOne {
+    public int[] plusOne(int[] digits) {
+    int last = digits.length-1;
+    if (last==0){
+        if(digits[last]<9){
+            digits[last]++;
+            return digits;
+        } return new int[]{1,0};
+    }
+
+        for(int i =last; i>=0; i--){
+            if(digits[i]==9){
+                digits[i]=0;
+            }else {
+                digits[i]++;
+                return digits;
+            }
+        }
+        int [] result = new int[last+2];
+        result[0]=1;
+
+        return result;
+    }
 }
